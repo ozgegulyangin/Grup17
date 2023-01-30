9f44b764dffe2c79752c2a125b2a5cb1c0200be1
Emircan <akyuzemircan390@gmail.com>
2023-01-30 22:14:01 +0300

    emir

--- src/Emircan.java
+++ src/Emircan.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+
 public class Emircan {
     public static void main(String[] args) {
 
-    }
-}
+                int [] dizi=new int[6];
+                for (int i = 0; i < dizi.length; i++) {
+                    dizi[i]=(int) (Math.random()*10);
+                }
+                System.out.println(Arrays.toString(dizi));
+
+            }
+        }
